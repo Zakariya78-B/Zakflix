b357f9b5af88dda8fa887f7ff09608ba10b4ec82
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-28 23:12:02 +0200

    routage vide => sipenner => affichage

--- zakflix/src/App.js
+++ zakflix/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
 
 import { Home } from './routes'
-import { Header } from './components';
-import { API_URL,API_KEY,IMAGE_BASE_URL,POSTER_SIZE,BACKDROP_SIZE} from './components/config'
+import { Header, Spinner } from './components';
+import { API_URL,API_KEY,IMAGE_BASE_URL,BACKDROP_SIZE} from './components/config'
 
 import './App.css';
 
@@ -11,49 +13,12 @@ import './App.css';
 
 class App extends Component {
   state ={
-    loading :false,
-    movies: [
-      {
-        backdrop_path: "./images/Fast_large.jpg",
-        id: 475557,
-        overview:
-          "Dans les années 1980, à Gotham City, Arthur Fleck, un humoriste de stand-up raté, bascule dans la folie et devient le Joker.",
-        poster_path: "./images/Fast_small.jpg",
-        title: "Joker"
-      },
-      {
-          backdrop_path: "./images/Fast_large.jpg",
-          id: 475558,
-          overview:
-            "Dans les années 1980, à Gotham City, Arthur Fleck, un humoriste de stand-up raté, bascule dans la folie et devient le Joker.",
-          poster_path: "./images/Fast_small.jpg",
-          title: "Joker"
-        },
-        {
-          backdrop_path: "./images/Fast_large.jpg",
-          id: 475559,
-          overview:
-            "Dans les années 1980, à Gotham City, Arthur Fleck, un humoriste de stand-up raté, bascule dans la folie et devient le Joker.",
-          poster_path: "./images/Fast_small.jpg",
-          title: "Joker"
-        },
-        {
-          backdrop_path: "./images/Fast_large.jpg",
-          id: 475550,
-          overview:
-            "Dans les années 1980, à Gotham City, Arthur Fleck, un humoriste de stand-up raté, bascule dans la folie et devient le Joker.",
-          poster_path: "./images/Fast_small.jpg",
-          title: "Joker"
-        },
-    ],
+    loading :true,
+    movies: [],
     badge: 0,
-    image: './images/Fast_large.jpg',
-    mTitle: "Fast and Furious",
-    mDescription: `La nuit tombée, Dominic Toretto règne sur les rues de Los Angeles à la tête d'une équipe 
-    de fidèles qui partagent son goût du risque, sa passion de la vitesse et son culte des voitures de sport
-     lancées à plus de 250 km/h dans des rodéos urbains d'une rare violence. Ses journées sont consacrées à bricoler 
-     et à relooker des modèles haut de gamme, à les rendre toujours plus performants et plus voyants, et à organiser
-      des joutes illicites.`,
+    image: null,
+    mTitle: "",
+    mDescription: "",
     activePage: 0,
     totalPages: 0,
     searchText:""
@@ -132,14 +97,30 @@ class App extends Component {
   }
   render(){
       return (
-    <div className="App">
-        <Header badge={this.state.badge} />
-        <Home 
-          {...this.state}
-          onSearchClick={this.handleSearch}
-          onButtonClick={this.loadMore}
-        />
-    </div>
+        <BrowserRouter>
+              <div className="App">
+          <Header badge={this.state.badge} />
+          {!this.state.image ?
+            (
+              <Spinner />
+            ):
+            (
+                <Switch>
+            <Route path="/" exact render={() => (
+              <Home 
+                        {...this.state}
+                        onSearchClick={this.handleSearch}
+                        onButtonClick={this.loadMore}
+                      />
+            )}
+          />   
+          </Switch>
+            )}
+        
+        
+          </div>
+        </BrowserRouter>
+
   );
   }
 
