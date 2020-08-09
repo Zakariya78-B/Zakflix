41709f87d3d05a4d31af95de7d340c1afb2bea41
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-08-09 22:07:03 +0200

    crevenue + conversion

--- zakflix/src/components/HeaderDetails.js
+++ zakflix/src/components/HeaderDetails.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Stars } from './index';
 import {IMAGE_BASE_URL,POSTER_SIZE} from '../components/config';
+import {calcTime,convertMoney} from '../utils/helpers';
+
 import '../css/HeaderDetails.css';
 
 class HeaderDetails extends Component{
@@ -16,9 +18,9 @@ class HeaderDetails extends Component{
                     <h3 className="headerDetails--container__title">{this.props.mTitle}</h3>
                     <p className="headerDetails--container__desc">{this.props.mDesc}</p>
                     <div className="headerDetails--info">
-                        <Container iconName="clock" content={this.props.runtime} />
+                        <Container iconName="clock" content={calcTime(this.props.runtime)} />
                         <Stars fakeArray1={["1","1","1"]} fakeArray2={["1","1"]}/>
-                        <Container iconName="money" content={this.props.revenue} />
+                        <Container iconName="money" content={convertMoney(this.props.revenue)} />
                         
                     </div>
                 </div>
