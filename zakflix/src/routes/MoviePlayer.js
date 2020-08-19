e8c19fcfa53a66b2e18a6f6283ebf72ad2d9fa27
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-08-19 21:57:57 +0200

    url movie  player api

--- zakflix/src/routes/MoviePlayer.js
+++ zakflix/src/routes/MoviePlayer.js
@@ -1,11 +1,14 @@
 import React, {Component } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 import { VideoPlayer,MvPlayerList,Spinner} from '../components/';
-import { API_KEY, API_URL } from '../config';
+import { API_KEY, API_URL, IMAGE_BASE_URL, BACKDROP_SIZE } from '../components/config';
+import { calcTime } from '../utils/helpers'
 
 import '../css/MoviePlayer.css';
 
+let newMovies = [];
 class MoviePlayer extends Component {
     state = {
         movies: [
@@ -59,21 +62,71 @@ class MoviePlayer extends Component {
              },
         loading:false        
     }
+    async componentDidMount(){
+        const oldMovies = JSON.parse(localStorage.getItem('movies'));
+        const results = await this.getNewMovies(oldMovies);
+        newMovies = oldMovies.map((oldMovie, index) =>{
+            return{
+                id: oldMovie.id,
+                position: index+1,
+                title: oldMovie.title,
+                duration: results[index],
+                imageUrl: `${IMAGE_BASE_URL}/${BACKDROP_SIZE}/${oldMovie.backdrop_path}`,
+                videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            }
+        })
+
+        const id = this.props.match.params.id;
+
+        if(id){
+            const selectedMovie = this.getSelectedMovie(newMovies, id);
+            this.setState({
+                loading:false,
+                movies: [...newMovies],
+                selectedMovie
+            })
+        }else {
+            const selectedMovie =newMovies[0];
+            this.setState({
+                loading:false,
+                movies: [...newMovies],
+                selectedMovie
+            })
+            this.props.history.push({
+                pathname:`/player/${selectedMovie.id}`
+            })
+        }
+
+    }
+    getSelectedMovie = (movies, movieId) => {
+        const selectedMovie = _.find(movies, { id : parseInt(movieId,10)});
+        return selectedMovie;
+    }
     handeleEnded = () =>{
         console.log('video ended');
     }
     getTime = movieId => {
         return new Promise((resolve,reject) =>{
-            const url = `${API_URL}/movies/${movieId}?api_key=${API_KEY}&language=fr`;
+            const url = `${API_URL}/movie/${movieId}?api_key=${API_KEY}&language=fr`;
             axios.get(url).then(data => {
-                const duration = data.data.duration;
-                resolve(duaration)
+                const duration = data.data.runtime;
+                resolve(duration)
             })
             .catch(e => {
                 console.log('e',e);
                 reject('error' , e);
             })
         })
+    }
+    getNewMovies = async oldMovies => {
+        let promises = [];
+        for (let i = 0; i < oldMovies.length;i++){
+            const element = oldMovies[i];
+            const id = element.id;
+            const time = await this.getTime(id);
+            promises.push(calcTime(time));
+        }
+        return Promise.all(promises);
     }
     render(){
         const { movies, selectedMovie } = this.state;
